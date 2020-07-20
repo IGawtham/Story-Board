93e62137d71e941e892082c0c53ebb29cbf041a6
Gawtham R <gawtham.r@ad.infosys.com>
2020-07-20 20:00:08 +0530

    Final Application

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const exphbs = require("express-handlebars");
+const methodOverride = require("method-override");
 const connectDB = require("./config/db");
 const passport = require("passport");
 const session = require("express-session");
@@ -13,6 +14,22 @@ dotenv.config({ path: "./config/config.env" });
 connectDB();
 const app = express();
 
+//Body Parser
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
+
 //Passport config
 require("./config/passport")(passport);
 
@@ -20,8 +37,25 @@ require("./config/passport")(passport);
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+//Handlebar helpers
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/hbs");
+
 //Handlebars
-app.engine(".hbs", exphbs({ extname: ".hbs", defaultLayout: "main" }));
+app.engine(
+  ".hbs",
+  exphbs({
+    helpers: { formatDate, stripTags, truncate, editIcon, select },
+    extname: ".hbs",
+    defaultLayout: "main",
+  })
+);
 app.set("view engine", ".hbs");
 
 //Sessions
@@ -37,13 +71,20 @@ app.use(
 //Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
+
+//set global variable
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null;
+  next();
+});
+
 //static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/", require("./routes/index"));
 app.use("/auth", require("./routes/auth"));
-
+app.use("/stories", require("./routes/stories"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
